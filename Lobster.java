37f10f9226ed2587b1203a30dfd1219bae2cee57
Suzana-2008 <suzanasilva2008hot@gmail.com>
2025-08-25 11:39:00 -0300

    Fine Decapota

---
+++ Lobster.java
@@ -0,0 +1,8 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Lobster extends Decapoda
+{
+    public Lobster(){        
+        super("A","D",550,550);        
+    }    
+}
